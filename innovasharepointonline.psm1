44e87adbf4ee0f855bf46d05c0b69e4e5892cbae
Derek Schauland <dschauland@weareinnova.com>
2017-02-10 16:02:29 -0600

    function updates and more help

--- innovasharepointonline.psm1
+++ innovasharepointonline.psm1
@@ -1,6 +1,7 @@
-﻿<#.EXTERNALHELP c:\Users\dschauland\Documents\SAPIEN\PowerShell Studio\Files\SPO\innovasharepointonline.psm1-Help.xml#>
+﻿
 function Migrate-INDocumentLibrary
 {
+	<#.EXTERNALHELP innovasharepointonline.psm1-Help.xml#>
 	
 	[cmdletbinding()]
 	param (
@@ -44,11 +45,6 @@ function Migrate-INDocumentLibrary
 		$files = get-pnpfolderitem -foldersiterelativeurl $folder -itemtype File
 	}
 	
-	
-	
-	
-	
-	
 	function time-now
 	{
 		$(Get-Date -Format "yyyy-MM-dd-hh-mm-ss")
@@ -68,6 +64,71 @@ function Migrate-INDocumentLibrary
 	Start-Log -LogPath $logfilepath -LogName "$logname" -ScriptVersion "1.0.0" | Out-Null
 	
 	
+	<#function split-array
+	{
+		param (
+			[object[]]$input,
+			[int]$splitsize
+		)
+		
+		$length = $input.Length
+		for ($index = 0; $index -lt $length; $index += $splitsize)
+		{
+			,($input[$index..($index+$splitsizw -1)])
+		}
+		
+	}#>
+	
+	function split-array
+	{
+		[CmdletBinding()]
+		param (
+			[Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+			$Collection,
+			[Parameter(Mandatory = $true)]
+			[ValidateRange(1, 247483647)]
+			[int]$Count)
+		begin
+		{
+			$Ctr = 0
+			$Array = @()
+			$TempArray = @()
+		}
+		process
+		{
+			foreach ($e in $Collection)
+			{
+				if (++$Ctr -eq $Count)
+				{
+					$Ctr = 0
+					$Array += , @($TempArray + $e)
+					$TempArray = @()
+					continue
+				}
+				$TempArray += $e
+			}
+		}
+		end
+		{
+			if ($TempArray) { $Array += , $TempArray }
+			$Array
+		}
+	}
+	
+	$filechunks = split-array -collection $files -count 100
+	
+	<#$partNum = 0
+	foreach ($part in $filechunks)
+	{
+		"Hello Part $partNum"
+		foreach ($thing in $part)
+		{
+			$thing
+		}
+		$partNum++
+	}
+	#>
+	
 	if (!$count)
 	{
 		if ($($files.count) -eq 0)
@@ -77,16 +138,23 @@ function Migrate-INDocumentLibrary
 		}
 		else
 		{
-			foreach ($file in $files)
-			{
-				$path = $folder + $file.name
-				$targetpath = $target + $file.name
-				
-				move-pnpfile -siterelativeurl $path -targeturl $targetpath -force
-				
-				Write-LogInfo -LogPath $fulllogpath -Message "[$(time-now)] Count Parameter Not Specified - Moved file: $path to $targetpath"
-			}
 			
+			foreach ($i in $filechunks.count)
+			{
+				foreach ($file in $filechunks[$i])
+				{
+					$source = $folder + "/" + $file.name
+					$targetpath = $target + "/" + $file.name
+					
+					move-pnpfile -siterelativeurl $source -targeturl $targetpath -force
+					
+					Write-Host "$source headed to $targetpath - inside loop" 
+					
+				}
+				
+				
+				Write-LogInfo -LogPath $fulllogpath -Message "[$(time-now)] Count Parameter Not Specified - Moved file: $source to $targetpath"
+			}
 			Write-verbose "$($files.count) files have been moved from $folder to $target"
 			Write-LogInfo -LogPath $fulllogpath -Message "[$(time-now)] Count Parameter Not Specified -  Moved $($files.count) from $folder to $target"
 			
@@ -109,4 +177,76 @@ function Migrate-INDocumentLibrary
 	
 	Stop-Log -LogPath $fulllogpath -NoExit
 	$ErrorActionPreference = 'Continue'
+}
+
+function Add-INDocuments
+{
+	<#.EXTERNALHELP Add-INDocuments-Help.xml#>
+	
+	param ([parameter(Mandatory)]
+		[string]$path,
+		[string]$library,
+		[switch]$progress
+	)
+	
+	$uploads = Get-ChildItem -Path $path
+	
+	$pnpSite = (get-pnpsite).url
+	$pnpweb = (get-pnpweb).serverrelativeurl
+	
+	Write-Host "Current SharePoint Online Environment: $pnpSite"
+	Write-Host "Current Site within SharePoint Online Environment: $pnpweb"
+	
+	if ($progress)
+	{
+		$x = 0
+		foreach ($file in $uploads)
+		{
+			$x++
+			
+			Write-Progress -Activity "Uploading Files" -Status "Percent Complete: " -PercentComplete (($x / $uploads.count) * 100)
+			add-pnpfile -path $file -folder $library
+		}
+	}
+	else
+	{
+		#$x = 0
+		foreach ($file in $uploads)
+		{
+			#$x++
+			
+			#Write-Progress -Activity "Uploading Files" -Status "Percent Complete: " -PercentComplete (($x / $uploads.count) * 100)
+			add-pnpfile -path $file -folder $library
+		}
+	}
+	
+}
+
+function New-INTestFiles
+{
+	param ([string]$path,
+		[int]$maxfiles,
+		[int]$startwith = 0,
+		[switch]$progress
+	)
+	
+	if ($progress)
+	{
+		for ($i = $startwith; $i -lt ($startwith + $maxfiles); $i++)
+		{
+			Write-Progress -Activity "Creating asdf Files" -Status "Percent Complete: " -PercentComplete (($i / ($startswith + $maxfiles).count) / 100)
+			$filepath = $path + "\asdf$i.txt"
+			Add-Content -Path $filepath -Value "asdf" | out-null
+		}
+	}
+	else
+	{
+		for ($i = $startwith; $i -lt ($startwith + $maxfiles); $i++)
+		{
+			#Write-Progress -Activity "Creating asdf Files" -Status "Percent Complete: " -PercentComplete (($i / ($startswith + $maxfiles).count) * 100)
+			$filepath = $path + "\asdf$i.txt"
+			Add-Content -Path $filepath -Value "asdf" | out-null
+		}
+	}
+	
 }
